2b5951afc702d453efa2fad2a021500fb93481f1
Gianpiero Rinzivillo <gianpiero.rinzivillo@gmail.com>
2017-01-26 09:35:30 +0100

    my submission solution to module1

---
+++ module1-solution/app.js
@@ -0,0 +1,53 @@
+(function(){
+  'use strict';
+
+  angular.module('LunchCheck',[])
+  .controller( 'LunchCheckController', LunchCheckController );
+
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope)
+  {
+
+    $scope.checkDishes = function () {
+      var dishes = $scope.lunchMenu;
+
+      // there are no meals?!
+      if ( !dishes ){
+        $scope.message   = "Please enter data first";
+        console.log("fill in meals");
+        return;
+      }
+      // let's get clean table before lunch
+      dishes = dishes.split(",");
+      dishes = cleanEmptyDish(dishes);
+
+      // look if I'm overweight
+      if ( dishes.length <= 3) {
+        // that's ok
+        $scope.message   = "Enjoy!";
+        $scope.cssStatus = "green";
+      } else {
+        // mmm the man who eat too much
+        $scope.message   = "Too much!";
+        $scope.cssStatus = "red";
+
+      }
+
+      console.log(dishes);
+
+    };
+  }
+
+  // look for valid dish. any bunch of chars that are not whitespaces.
+  function cleanEmptyDish(dishes)
+  {
+    var newDishes = [];
+    for (var i = 0; i < dishes.length; i++) {
+       if (dishes[i].trim().length > 0) newDishes.push( dishes[i] );
+    }
+    return newDishes;
+  }
+
+
+})();
